969b3895d1f4ce13c7fff25ed3f283360dc283a8
MrTime <kfedia@gmail.com>
2011-10-30 17:29:55 +0200

    shader uniforms parsing

--- challenger.js
+++ challenger.js
@@ -31,10 +31,11 @@ function NodeFactory() {
 }
 
 /**
-	WebGL shader program object
+	WebGL shader program object.
+	Links WebGL program and gets locations for all uniforms.
 */
-function ShaderProgram() {
-
+function ShaderProgram(title, shaders) {
+	
 }
 
 /**
@@ -42,7 +43,7 @@ function ShaderProgram() {
 	Makes shader program from few shaders
 */
 function ShaderProgramFactory() {
-
+	
 }
 
 /**
@@ -113,8 +114,8 @@ function TextureFactory() {
 *	Base class for shader-based objects.
 *	Container for shader variables values. 
 */
-function FX() {
-
+function FX(source) {
+	
 }
 
 /**
@@ -123,17 +124,83 @@ function FX() {
 	\param source hash in format {:name,:type,:glsl}, where type can be "vertex", "fragment"
 */
 function Shader(source) {
+	// free WebGL object
+	this.clean = function() {
+		gl.destroyShader(this.obj);
+		this.parameters = [];
+	}
+	
+	this.uniformMatrix2 = function(location, value) {
+		gl.uniformMatrix2fv(location, (value.transpose == true), value);
+	}
+	
+	this.uniformMatrix3 = function(location, value) {
+		gl.uniformMatrix3fv(location, (value.transpose == true), value);
+	}
+	
+	this.uniformMatrix4 = function(location, value) {
+		gl.uniformMatrix4fv(location, (value.transpose == true), value);
+	}
+	
+	// determinate set function for specific argument type
+	function setter_for(typename) {
+		switch (typename) {
+			case "float":	return gl.uniform1f; break;
+			case "vec2":	return gl.uniform2fv; break;
+			case "vec3":	return gl.uniform3fv; break;
+			case "vec4":	return gl.uniform4fv; break;
+			case "sample2D":
+			case "sampleCube":
+			case "bool":
+			case "int":		return gl.uniform1i; break;
+			case "bvec2":	
+			case "ivec2":	return gl.uniform2iv; break;
+			case "bvec3":	
+			case "ivec3":	return gl.uniform3iv; break;
+			case "bvec4":	
+			case "ivec4":	return gl.uniform4iv; break;
+			case "mat2":	return this.uniformMatrix2; break;	
+			case "mat3":	return this.uniformMatrix3; break;	
+			case "mat4":	return this.uniformMatrix4; break;	
+			default:		return null;
+		}
+	}
+
 	this.name = source.name;
+	this.dependency = source.dependency;
+	
+	// compile shader
 	this.obj = gl.createShader(source.type == "vertex" ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER);
+	
 	gl.shaderSource(this.obj, source.glsl);
 	gl.compileShader(this.obj);
 
 	if (!gl.getShaderParameter(this.obj, gl.COMPILE_STATUS)) {
 		alert(gl.getShaderInfoLog(shader));
+		return;
 	}
 	
-	this.clean = function() {
-		gl.destroyShader(this.obj);
+	// parse uniform and connect with set functions
+	var gen_uniform_regexp = /uniform\s+\w+\s+\w+[^;]*/ig;
+	var uniform_regexp = /uniform\s+(\w+)\s+(\w+)(\s+=\s+(.*))*/i;
+	var uniforms = source.glsl.match(gen_uniform_regexp);
+	this.parameters = [];
+	
+	for (var i = 0; i < uniforms.length; i++) {
+		var uniform_src = uniforms[i];
+		
+		// 1 - uniform type
+		// 2 - uniform name
+		// 4 - uniform default
+		var values = uniform_src.match(uniform_regexp)
+		
+		var parameter = new Object();
+		parameter.name = values[2];
+		parameter.setter = setter_for(values[1]);
+		
+		//TODO: process uniform default value
+			
+		this.parameters.push(parameter);
 	}
 } 
 
