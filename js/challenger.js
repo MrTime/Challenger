450f4fef10e1ef5b437365b98e1dfc2756a3838d
Fedir Kovbel <kfedia@gmail.com>
2011-11-13 22:53:59 +0200

    rewrite to coffee script

---
+++ js/challenger.js
@@ -0,0 +1,257 @@
+var Challenger, FX, ResourceFactory, Shader, ShaderFactory, ShaderProgram, ShaderProgramFactory;
+var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+Challenger = (function() {
+
+  function Challenger() {
+    this.shaderFactory = new ShaderFactory;
+    this.shaderProgramFactory = new ShaderProgramFactory;
+  }
+
+  return Challenger;
+
+})();
+
+ResourceFactory = (function() {
+
+  function ResourceFactory() {
+    this.cache = {};
+  }
+
+  ResourceFactory.prototype.create = function(source) {
+    var full_name;
+    full_name = this.instance_name(source);
+    if (this.cache[full_name]) {
+      return this.cache[full_name];
+    } else {
+      return this.cache[full_name] = this.create_new(source);
+    }
+  };
+
+  ResourceFactory.prototype.free = function(instance) {
+    instance.clean;
+    return this.cache[instance.name] = null;
+  };
+
+  ResourceFactory.prototype.instance_name = function(source) {
+    return source.name;
+  };
+
+  ResourceFactory.prototype.create_new = function(source) {};
+
+  return ResourceFactory;
+
+})();
+
+Shader = (function() {
+
+  function Shader(source) {
+    var gen_uniform_regexp, parameters, uniform_regexp, uniform_src, uniforms, values, _i, _len;
+    this.name = source.name;
+    this.dependency = source.dependency;
+    this.glsl = source.glsl;
+    this.type = source.type === "vertex" ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER;
+    gen_uniform_regexp = /uniform\s+\w+\s+\w+[^;]*/ig;
+    uniform_regexp = /uniform\s+(\w+)\s+(\w+)(\s+=\s+(.*))*/i;
+    uniforms = source.glsl.match(gen_uniform_regexp);
+    this.parameters = [];
+    if (uniforms) {
+      for (_i = 0, _len = uniforms.length; _i < _len; _i++) {
+        uniform_src = uniforms[_i];
+        values = uniform_src.match(uniform_regexp);
+        parameters = {
+          name: values[2],
+          setter: this.setter_for(values[1])
+        };
+        this.parameters.push(parameters);
+      }
+    }
+  }
+
+  Shader.prototype.clean = function() {
+    gl.destroyShader(this.obj);
+    return this.parameters = [];
+  };
+
+  Shader.prototype.uniformMatrix2 = function(location, value) {
+    return gl.uniformMatrix2fv(location, value.transpose === true, value);
+  };
+
+  Shader.prototype.uniformMatrix3 = function(location, value) {
+    return gl.uniformMatrix3fv(location, value.transpose === true, value);
+  };
+
+  Shader.prototype.uniformMatrix4 = function(location, value) {
+    return gl.uniformMatrix4fv(location, value.transpose === true, value);
+  };
+
+  Shader.prototype.setter_for = function(typename) {
+    switch (typename) {
+      case "float":
+        return gl.uniform1f;
+      case "vec2":
+        return gl.uniform2fv;
+      case "vec3":
+        return gl.uniform3fv;
+      case "vec4":
+        return gl.uniform4fv;
+      case "sample2D":
+      case "sampleCube":
+      case "bool":
+      case "int":
+        return gl.uniform1i;
+      case "bvec2":
+      case "ivec2":
+        return gl.uniform2iv;
+      case "bvec3":
+      case "ivec3":
+        return gl.uniform3iv;
+      case "bvec4":
+      case "ivec4":
+        return gl.uniform4iv;
+      case "mat2":
+        return this.uniformMatrix2;
+      case "mat3":
+        return this.uniformMatrix3;
+      case "mat4":
+        return this.uniformMatrix4;
+      default:
+        return null;
+    }
+  };
+
+  return Shader;
+
+})();
+
+ShaderFactory = (function() {
+
+  __extends(ShaderFactory, ResourceFactory);
+
+  function ShaderFactory() {
+    ShaderFactory.__super__.constructor.apply(this, arguments);
+  }
+
+  ShaderFactory.prototype.create_new = function(source) {
+    return new Shader(source);
+  };
+
+  return ShaderFactory;
+
+})();
+
+ShaderProgram = (function() {
+
+  function ShaderProgram(name, shaders) {
+    var fragmentShader, fullFragmentGLSL, fullVertexGLSL, parameter, shader, shaderSource, vertexShader, _i, _j, _k, _len, _len2, _len3, _ref;
+    this.name = name;
+    fullVertexGLSL = "";
+    fullFragmentGLSL = "";
+    this.obj = gl.createProgram();
+    for (_i = 0, _len = shaders.length; _i < _len; _i++) {
+      shaderSource = shaders[_i];
+      if (shaderSource.type === gl.VERTEX_SHADER) {
+        fullVertexGLSL += shaderSource.glsl;
+      } else {
+        fullFragmentGLSL += shaderSource.glsl;
+      }
+    }
+    vertexShader = this.compile(fullVertexGLSL, gl.VERTEX_SHADER);
+    fragmentShader = this.compile(fullFragmentGLSL, gl.FRAGMENT_SHADER);
+    gl.attachShader(this.obj, vertexShader);
+    gl.attachShader(this.obj, fragmentShader);
+    gl.linkProgram(this.obj);
+    if (!gl.getProgramParameter(this.obj, gl.LINK_STATUS)) {
+      alert("Could not initialize shaders");
+    }
+    this.use();
+    this.uniforms = {};
+    for (_j = 0, _len2 = shaders.length; _j < _len2; _j++) {
+      shader = shaders[_j];
+      _ref = shader.parameters;
+      for (_k = 0, _len3 = _ref.length; _k < _len3; _k++) {
+        parameter = _ref[_k];
+        this.uniforms[parameter.name] = gl.getUniformLocation(this.obj, parameter.name);
+      }
+    }
+  }
+
+  ShaderProgram.prototype.clean = function() {
+    gl.destroyProgram(this.obj);
+    return this.uniforms = null;
+  };
+
+  ShaderProgram.prototype.use = function() {
+    return gl.useProgram(this.obj);
+  };
+
+  ShaderProgram.prototype.compile = function(glsl, type) {
+    var shader;
+    shader = gl.createShader(type);
+    gl.shaderSource(shader, glsl);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+    }
+    return shader;
+  };
+
+  return ShaderProgram;
+
+})();
+
+ShaderProgramFactory = (function() {
+
+  __extends(ShaderProgramFactory, ResourceFactory);
+
+  function ShaderProgramFactory() {
+    ShaderProgramFactory.__super__.constructor.apply(this, arguments);
+  }
+
+  ShaderProgramFactory.prototype.instance_name = function(source) {
+    var name, shader, _i, _len;
+    name = "";
+    for (_i = 0, _len = source.length; _i < _len; _i++) {
+      shader = source[_i];
+      name += shader.name + "|";
+    }
+    return name;
+  };
+
+  ShaderProgramFactory.prototype.create_new = function(source) {
+    return new ShaderProgram(this.instance_name(source), source);
+  };
+
+  return ShaderProgramFactory;
+
+})();
+
+FX = (function() {
+
+  function FX(source) {
+    var parameters, shader, shaderName, shaderSource, _len, _ref;
+    this.title = source.name;
+    this.parameters = {};
+    this.shaders = {};
+    _ref = source.shaders;
+    for (shaderSource = 0, _len = _ref.length; shaderSource < _len; shaderSource++) {
+      shaderName = _ref[shaderSource];
+      shader = ShaderFactory.create(shaderSource);
+      parameters = parameters.concat(shader.parameters);
+      this.shaders[shaderName] = shader;
+    }
+  }
+
+  FX.prototype.apply = function(program) {
+    var parameter, parameterName, _len, _results;
+    _results = [];
+    for (parameter = 0, _len = parameters.length; parameter < _len; parameter++) {
+      parameterName = parameters[parameter];
+      _results.push(parameter.setter(program.uniforms[parameterName], param.value));
+    }
+    return _results;
+  };
+
+  return FX;
+
+})();
---
+++ src/challenger.coffee
@@ -0,0 +1,155 @@
+class Challenger
+  constructor: ->
+    @shaderFactory = new ShaderFactory
+    @shaderProgramFactory = new ShaderProgramFactory
+    
+class ResourceFactory
+  constructor: ->
+    @cache = {}
+    
+  create: (source) ->
+    full_name = @instance_name(source)
+    if @cache[full_name]
+      @cache[full_name]
+    else
+      @cache[full_name] = @create_new(source)  
+      
+    
+  free: (instance) ->
+    instance.clean
+    @cache[instance.name] = null
+  
+  instance_name: (source) ->
+    source.name
+  
+  create_new: (source) ->
+    # abstract
+    
+class Shader
+  constructor: (source) ->
+    @name = source.name
+    @dependency = source.dependency
+    @glsl = source.glsl
+    @type = if (source.type == "vertex") then gl.VERTEX_SHADER else gl.FRAGMENT_SHADER
+    
+    gen_uniform_regexp = /uniform\s+\w+\s+\w+[^;]*/ig
+    uniform_regexp = /uniform\s+(\w+)\s+(\w+)(\s+=\s+(.*))*/i
+    uniforms = source.glsl.match(gen_uniform_regexp)
+    @parameters = []
+    
+    if uniforms
+      for uniform_src in uniforms
+        # 1 - uniform type
+        # 2 - uniform name
+        # 4 - uniform default
+        values = uniform_src.match(uniform_regexp)
+        
+        parameters = 
+          name: values[2]
+          setter: @setter_for(values[1])
+        
+        @parameters.push parameters
+        
+  clean: ->
+    gl.destroyShader(this.obj);
+    @parameters = []
+    
+  uniformMatrix2: (location, value) -> 
+    gl.uniformMatrix2fv(location, (value.transpose == true), value);
+  
+  uniformMatrix3: (location, value) -> 
+    gl.uniformMatrix3fv(location, (value.transpose == true), value);
+  
+  uniformMatrix4: (location, value) -> 
+    gl.uniformMatrix4fv(location, (value.transpose == true), value);
+    
+  setter_for: (typename) ->
+    switch typename
+      when "float" then gl.uniform1f
+      when "vec2"  then gl.uniform2fv
+      when "vec3"  then gl.uniform3fv
+      when "vec4"  then gl.uniform4fv
+      when "sample2D","sampleCube","bool","int" then gl.uniform1i
+      when "bvec2","ivec2" then gl.uniform2iv
+      when "bvec3","ivec3" then gl.uniform3iv
+      when "bvec4","ivec4" then gl.uniform4iv
+      when "mat2" then @uniformMatrix2
+      when "mat3" then @uniformMatrix3
+      when "mat4" then @uniformMatrix4
+      else null
+      
+class ShaderFactory extends ResourceFactory  
+  constructor: ->
+    super
+ 
+  create_new: (source) -> new Shader(source)     
+      
+class ShaderProgram
+  constructor: (@name, shaders) ->
+    fullVertexGLSL = ""
+    fullFragmentGLSL = ""
+    @obj = gl.createProgram()
+    for shaderSource in shaders
+      if shaderSource.type == gl.VERTEX_SHADER then fullVertexGLSL += shaderSource.glsl
+      else fullFragmentGLSL += shaderSource.glsl
+    
+    vertexShader = @compile(fullVertexGLSL, gl.VERTEX_SHADER)
+    fragmentShader = @compile(fullFragmentGLSL, gl.FRAGMENT_SHADER)
+    
+    gl.attachShader(@obj, vertexShader);
+    gl.attachShader(@obj, fragmentShader);
+    gl.linkProgram(@obj)
+    
+    unless gl.getProgramParameter(@obj, gl.LINK_STATUS) 
+      alert("Could not initialize shaders");
+    
+    @use();
+
+    # get uniforms locations
+    @uniforms = {}
+    for shader in shaders
+      for parameter in shader.parameters
+        @uniforms[parameter.name] = gl.getUniformLocation(@obj, parameter.name)
+    
+  clean: ->
+    gl.destroyProgram(@obj)
+    @uniforms = null
+    
+  use: -> gl.useProgram(@obj)
+  
+  compile: (glsl,type) ->
+    shader = gl.createShader(type)
+    gl.shaderSource(shader, glsl)
+    gl.compileShader(shader)
+    
+    unless gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+      alert(gl.getShaderInfoLog(shader))
+      
+    shader
+      
+class ShaderProgramFactory extends ResourceFactory  
+  constructor: ->
+    super
+ 
+  instance_name: (source) ->
+    name = ""
+    for shader in source 
+      name += shader.name + "|"
+    name   
+ 
+  create_new: (source) -> new ShaderProgram(@instance_name(source), source)           
+     
+class FX
+  constructor: (source) ->
+    @title = source.name
+    @parameters = {}
+    @shaders = {}
+    
+    for shaderName, shaderSource in source.shaders
+      shader = ShaderFactory.create(shaderSource)
+      parameters = parameters.concat(shader.parameters)
+      @shaders[shaderName] = shader 
+      
+  apply: (program) ->
+    for parameterName, parameter in parameters
+        parameter.setter(program.uniforms[parameterName], param.value) 
